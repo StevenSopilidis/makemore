5355e44ef2548e937589e973fca1d9eb015c5111
StevenSopilidis <sopilidimaria@gmail.com>
2024-05-27 11:23:33 +0300

    refactored and added batchn normalization

--- mlp.py
+++ mlp.py
@@ -10,6 +10,7 @@ stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
 block_size = 3 # how many characters we take into account to predict the next one
+vocab_size = len(itos)
 
 def build_dataset(words):
     X, y = [], []
@@ -37,72 +38,95 @@ X_test, y_test = build_dataset(words[n2:])
 
 
 # create embeding table
-g = torch.Generator().manual_seed(32490123412)
-C = torch.randn((27, 10), generator=g)
+n_embed = 10
+n_hidden = 200
 
-W1 = torch.randn((30, 200), generator=g)
-W2 = torch.randn((200, 27), generator=g)
-b1 = torch.rand(200, generator=g)
-b2 = torch.randn(27, generator=g)
-parameters = [C, W1, b1, W2, b2]
+g = torch.Generator().manual_seed(32490123412)
+C = torch.randn((vocab_size, n_embed), generator=g)
+
+# initialize so standard_deviation is gain/sqrt(fan_mode) (he initialization)
+W1 = torch.randn((n_embed * block_size, n_hidden), generator=g) * (5/3)/((n_embed*block_size) ** 0.5)
+# b1 = torch.rand(n_hidden, generator=g) * 0.01
+W2 = torch.randn((n_hidden, vocab_size), generator=g) * (5/3)/(n_hidden** 0.5)
+b2 = torch.randn(vocab_size, generator=g) * 0
+
+bngain = torch.ones((1, n_hidden)) # batch_normalization gain
+bnbias = torch.zeros((1, n_hidden)) # batch_normalization bias
+
+bnmean_running = torch.zeros((1, n_hidden)) 
+bnstd_running = torch.ones((1, n_hidden))
+
+parameters = [C, W1, W2, b2, bngain, bnbias]
 
 for p in parameters:
     p.requires_grad = True
 
-lre = torch.linspace(-3, 0, 1000)
-lrs = 10**lre # learning rates to search over
+max_steps = 100_000
+batch_size = 32
 
-lri = []
-lossi = []
-stepi = []
-
-for i in range(30000):
+for i in range(max_steps):
     # minibatch construct
-    idx = torch.randint(low=0, high=X_train.shape[0], size=(32, ))
+    idx = torch.randint(low=0, high=X_train.shape[0], size=(batch_size, ))
 
     # forward pass
     embed = C[X_train[idx]]
-    h = torch.tanh(embed.view(embed.shape[0], embed.shape[1] * embed.shape[2]) @ W1 + b1)
+    embcat = embed.view(embed.shape[0], embed.shape[1] * embed.shape[2])
+    hpreact = embcat @ W1 #+ b1
+    # normalize hpreact (for batch normalization)
+    bnmeani = hpreact.mean(0, keepdim=True)
+    bnstdi = hpreact.std(0, keepdim=True) 
+    hpreact = bngain * (hpreact - bnmeani) / bnstdi + bnbias
+
+    with torch.no_grad():
+        # calculate mean and std of training set on the go
+        bnmean_running = 0.999 * bnmean_running + 0.001 * bnmeani        
+        bnstd_running = 0.999 * bnstd_running + 0.001 * bnstdi
+    
+    h = torch.tanh(hpreact)
     logits = h @ W2 + b2
     loss = F.cross_entropy(logits, y_train[idx])
+    
     # backwards pass
     for p in parameters:
         p.grad = None
     loss.backward()
 
-    # lr = lrs[i]
-    lr = 0.1
+    lr = 0.1 if i < 100_000 else 0.01
     for p in parameters:
-        p.data += -0.1 * p.grad
-    # print(f"---> Epoch: {i}, Loss: {loss}")
+        p.data += -lr * p.grad
 
-    # track stats
-    # lri.append(lre[i])
-    # lossi.append(loss.log10().item())
-    # stepi.append(i)
 
-# for finding the ideal learning_rate
-# plt.plot(lri, lossi)
-# plt.show()
-    
-# evaluated valid loss
-embed = C[X_valid]
-h = torch.tanh(embed.view(embed.shape[0], embed.shape[1] * embed.shape[2]) @ W1 + b1)
-logits = h @ W2 + b2
-valid_loss = F.cross_entropy(logits, y_valid)
-# print(f"Validation loss: {valid_loss}")
+@torch.no_grad()
+def split_loss(split):
+    X,y = {
+        "train": (X_train, y_train),
+        "valid": (X_valid, y_valid),
+        "test": (X_test, y_test)
+    }[split]
+    embed = C[X]
+    embcat = embed.view(embed.shape[0], embed.shape[1] * embed.shape[2])
+    hpreact = embcat @ W1 #+ b1
+    hpreact = bngain * (hpreact - bnmean_running) / bnstd_running + bnbias
+    h = torch.tanh(hpreact)
+    logits = h @ W2 + b2
+    loss = F.cross_entropy(logits, y)
+    print(f"{split}_loss: ", loss.item())
+
+split_loss('train')
+split_loss('valid')
 
 
 # Generate samples
 g = torch.Generator().manual_seed(23490932421)
-
+print("-------------------")
+print("Examples of names: ")
 for _ in range(10):
     out = []
     context = [0] * block_size
     while True:
         # forward pass
         embed = C[torch.tensor([context])]
-        h = torch.tanh(embed.view(embed.shape[0], embed.shape[1] * embed.shape[2]) @ W1 + b1)
+        h = torch.tanh(embed.view(embed.shape[0], embed.shape[1] * embed.shape[2]) @ W1)
         logits = h @ W2 + b2
         probs = F.softmax(logits, dim=1)
         idx = torch.multinomial(probs, num_samples=1, generator=g).item()
